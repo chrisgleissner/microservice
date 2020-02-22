93c72b5f6dab89dd711674495b1c935471bcaf95
Christian Gleissner <chrisgleissner@gmail.com>
2020-02-22 13:57:46 +0000

    Relocated embedded Maven

---
+++ quarkus/src/main/java/com/github/chrisgleissner/microservice/ping/PingResource.java
@@ -0,0 +1,20 @@
+package com.github.chrisgleissner.microservice.ping;
+
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("ping")
+public class PingResource {
+
+    @Inject
+    PingService pingService;
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public String hello() {
+        return "{\"pong\":\"" + pingService.currentTimeMillis() + "\"}";
+    }
+}
---
+++ quarkus/src/test/java/com/github/chrisgleissner/microservice/ping/PingResourceTest.java
@@ -0,0 +1,21 @@
+package com.github.chrisgleissner.microservice.ping;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.get;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.blankString;
+import static org.hamcrest.Matchers.not;
+
+@QuarkusTest
+class PingResourceTest {
+
+    @Test
+    void ping() {
+        get("/api/ping").then()
+                .statusCode(200)
+                .assertThat().body("pong", is(not(blankString())));
+    }
+}
