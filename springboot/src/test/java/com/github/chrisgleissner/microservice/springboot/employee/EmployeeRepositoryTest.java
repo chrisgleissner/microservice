be4371ac57d15eb2b8726547463b10f6072d5035
Christian Gleissner <chrisgleissner@gmail.com>
2020-02-23 20:44:22 +0000

    Added repository test

---
+++ springboot/src/test/java/com/github/chrisgleissner/microservice/springboot/employee/EmployeeRepositoryTest.java
@@ -0,0 +1,22 @@
+package com.github.chrisgleissner.microservice.springboot.employee;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(SpringExtension.class)
+@DataJpaTest
+class EmployeeRepositoryTest {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @Test
+    void findAll() {
+        assertThat(employeeRepository.findAll()).isNotEmpty();
+    }
+}
