dccf59284b73c5b75a153d7d634e6b5712c6bb9a
Christian Gleissner <chrisgleissner@gmail.com>
2020-02-25 22:09:58 +0000

    Added native tests

---
+++ quarkus/src/test/java/com/github/chrisgleissner/microservice/quarkus/employee/NativeEmployeeResourceIT.java
@@ -0,0 +1,7 @@
+package com.github.chrisgleissner.microservice.quarkus.employee;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeEmployeeResourceIT extends EmployeeResourceTest{
+}
--- quarkus/src/test/java/com/github/chrisgleissner/microservice/quarkus/ping/NativePingResourceIT.java
+++ quarkus/src/test/java/com/github/chrisgleissner/microservice/quarkus/ping/NativePingResourceIT.java
@@ -4,6 +4,4 @@ import io.quarkus.test.junit.NativeImageTest;
 
 @NativeImageTest
 public class NativePingResourceIT extends PingResourceTest {
-
-    // Execute the same tests but in native mode.
 }
